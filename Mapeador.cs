08d84350f6401c555e4f01dcd2d9d3e02f9c9970
Daniel <ramos.danielferreira@gmail.com>
2017-10-06 11:54:41 -0300

    Added beta support on IEnumerable<T>.
    Added beta support on IEnumerable<T>. Not working on not generics IEnumerables.

--- Mapeador.cs
+++ Mapeador.cs
@@ -5,9 +5,7 @@ namespace Mapeador
     /// Atenção: Não mapeia IEnumerable
     /// </summary>
 
-    public static class Mapeador
-    {
-        public static TResultado MapearPara<TOrigem, TResultado>(TOrigem origem)
+   public static TResultado MapearPara<TOrigem, TResultado>(TOrigem origem)
             where TResultado : class
             where TOrigem : class
         {
@@ -18,11 +16,15 @@ namespace Mapeador
 
             var propriedadesOrigem = tipoOrigem.GetProperties();
             var propriedadesDestino = tipoDestino.GetProperties();
+            var tiposNaoSuportados = new[] { typeof(List<>), typeof(IList<>), typeof(ICollection<>), typeof(IEnumerable<>) };
 
-            var resultado = Activator.CreateInstance(typeof(TResultado));
+            var resultado = typeof(TResultado) == typeof(string) ? origem : Activator.CreateInstance(typeof(TResultado));
+
             foreach (var propDestino in propriedadesDestino)
             {
+#if DEBUG
                 var isMapeada = false;
+#endif
                 if (propriedadesOrigem.Any(p => string.Compare(p.Name, propDestino.Name, StringComparison.InvariantCultureIgnoreCase) == 0 &&
                     p.PropertyType.Name == propDestino.PropertyType.Name))
                 {
@@ -33,25 +35,71 @@ namespace Mapeador
                         propriedadesDestino.First(
                             p => string.Compare(p.Name, propDestino.Name, StringComparison.InvariantCultureIgnoreCase) == 0 && p.GetType() == propDestino.GetType());
                     var podeSerEscrita = propriedadeEncontradaNoDestino.CanWrite &&
-                                         (!propDestino.PropertyType.IsGenericType || propDestino.PropertyType.GetGenericTypeDefinition() != typeof (IEnumerable<>) &&
+                                         (!propDestino.PropertyType.IsGenericType || propDestino.PropertyType.GetGenericTypeDefinition() != typeof(IEnumerable<>) &&
                                           propDestino.PropertyType.IsGenericType &&
-                                          propDestino.PropertyType.GetGenericTypeDefinition() != typeof (List<>));
+                                          !tiposNaoSuportados.Contains(propDestino.PropertyType.GetGenericTypeDefinition()));
                     if (podeSerEscrita)
                     {
-                        var valor = origem.GetType()
-                            .GetProperty(propriedadeEncontradaNaOrigem.Name)
-                            .GetValue(origem, null);
-                        propDestino.SetValue(resultado, valor, null);
+                        var propertyInfo = origem.GetType()
+                            .GetProperty(propriedadeEncontradaNaOrigem.Name);
+                        if (propertyInfo != null)
+                        {
+                            var valor = propertyInfo
+                                .GetValue(origem, null);
+                            propDestino.SetValue(resultado, valor, null);
+                        }
+#if DEBUG
                         isMapeada = true;
+#endif
                     }
+
+#if DEBUG
+                    isMapeada = podeSerEscrita;
+#endif
                 }
-#if(DEBUG)
+#if DEBUG
                 if (!isMapeada)
-                    Debug.WriteLine("ATENÇÃO: Propriedade {0} do tipo {1} não mapeada", propDestino.Name, tipoDestino.Name);
+                    Debug.WriteLine("ATENÇÃO: Propriedade {0} do tipo {1} não mapeada".ToUpperInvariant(), propDestino.Name, tipoDestino.Name);
 #endif
             }
 
             return resultado as TResultado;
         }
-    }
+
+        /// <summary>
+        /// Mapea Enumerable. Está com constraint para class
+        /// Refatorar para adicionar isto ao mapeador e ser recursivo
+        /// </summary>
+        /// <typeparam name="TOrigem"></typeparam>
+        /// <typeparam name="TDestino"></typeparam>
+        /// <param name="listaOrigem"></param>
+        /// <returns></returns>
+        public static IEnumerable<TDestino> MapearEnumerable<TOrigem, TDestino>(IEnumerable<TOrigem> listaOrigem)
+            where TDestino : class
+            where TOrigem : class
+        {
+            return MapearEnumerable<TOrigem, TDestino>(listaOrigem, null);
+        }
+
+        public static IEnumerable<TDestino> MapearEnumerable<TOrigem, TDestino>(IEnumerable<TOrigem> listaOrigem, Action<TDestino, TOrigem> customMapeador)
+            where TDestino : class
+            where TOrigem : class
+        {
+            var resultado = new List<TDestino>();
+            foreach (var itemOrigem in listaOrigem)
+            {
+                var tmp = Mapeador.MapearPara<TOrigem, TDestino>(itemOrigem);
+
+                if (customMapeador != null)
+                {
+                    customMapeador(tmp, itemOrigem);
+                }
+
+                resultado.Add(tmp);
+
+                
+            }
+
+            return resultado;
+        }
 }
